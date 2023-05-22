7ea18a36d016f4df236669fbdcb6a98a7efe4f90
Аня <grigorevna.antoninka@mail.ru>
2023-05-22 12:58:05 +0700

    Initial commit

---
+++ src/test/java/com/example/servis/ServisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
