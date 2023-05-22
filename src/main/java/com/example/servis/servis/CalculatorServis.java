b261d695a2986f99dc94392a480f797f76b04978
Аня <grigorevna.antoninka@mail.ru>
2023-05-22 18:39:50 +0700

    домашняя тесты.

--- src/main/java/com/example/servis/servis/CalculatorServis.java
+++ src/main/java/com/example/servis/servis/CalculatorServis.java
@@ -17,7 +17,6 @@ public class CalculatorServis {
             throw new DivByZeroException();
 
         }
-
         return (double)a/b;
     }
 
--- src/test/java/com/example/servis/servis/CalculatorServisParameterTest.java
+++ src/test/java/com/example/servis/servis/CalculatorServisParameterTest.java
@@ -65,7 +65,6 @@ public class CalculatorServisParameterTest {
     }
 
 
-
     @ParameterizedTest
     @MethodSource("podelitNegativTestParams")
     public void podelitNegativTest(int a,int b ){
